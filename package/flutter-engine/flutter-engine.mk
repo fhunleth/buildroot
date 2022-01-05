bd54006d377280195e82646771c1a827460dae7c
Frank Hunleth <fhunleth@troodon-software.com>
2022-01-05 17:33:40 -0500

    More flutter-engine WIP

--- package/flutter-engine/flutter-engine.mk
+++ package/flutter-engine/flutter-engine.mk
@@ -1,3 +1,19 @@
+################################################################################
+#
+# flutter-engine
+#
+#################################################################################
+
+# Flutter 2.8.1 (stable channel)
+FLUTTER_ENGINE_VERSION = 890a5fca2e34db413be624fc83aeea8e61d42ce6
+FLUTTER_ENGINE_SITE = $(call github,flutter,engine,$(FLUTTER_ENGINE_VERSION))
+FLUTTER_ENGINE_DEPENDENCIES = host-ninja host-python3
+FLUTTER_ENGINE_INSTALL_STAGING = YES
+
+FLUTTER_ENGINE_LICENSE = BSD-3-Clause
+FLUTTER_ENGINE_LICENSE_FILES = LICENSE
+
+
 GN_TARGET_OS = "linux"
 GN_TARGET_ARCH = "arm64"
 
@@ -7,40 +23,57 @@ GN_ARGS_append = " --linux-cpu ${GN_TARGET_ARCH}"
 GN_ARGS_append = " --arm-float-abi hard"
 GN_ARGS_append = " --embedder-for-target"
 GN_ARGS_append = " --disable-desktop-embeddings"
-FLUTTER_ENGINE_SITE=
-FLUTTER_ENGINE_SOURCE=
-FLUTTER_ENGINE_VERSION=da768751d43b1f287bf99bea703ea13e2eedcf4d
 
-FLUTTER_ENGINE_CONFIGURE_CMDS=
-		export DEPOT_TOOLS_UPDATE=0
-    export PATH=${S}:${S}/${GN_TOOLS_PYTHON2_PATH}:$PATH
+FLUTTER_ENGINE_DOWNLOAD_CMDS = \
+	echo "HELLO!!!!!"; \
+	echo 'solutions = [ \
+	    { \
+	        "managed" : False, \
+	        "name" : "src/flutter", \
+	        "url" : "https://github.com/flutter/engine.git@$(FLUTTER_ENGINE_VERSION)", \
+	        "custom_deps": {}, \
+	        "deps_file": "DEPS", \
+	        "safesync_url": "", \
+	        "custom_vars": { \
+	            "download_android_deps": False, \
+	            "download_windows_deps": False, \
+	        }, \
+	    }, \
+	]' > $(D)/.gclient;
 
-    cd ${WORKDIR}
-    echo 'solutions = [
-        {
-            "managed" : False,
-            "name" : "src/flutter",
-            "url" : "https://github.com/flutter/engine.git@${ENGINE_VERSION}",
-            "custom_deps": {},
-            "deps_file": "DEPS",
-            "safesync_url": "",
-            "custom_vars": {
-                "download_android_deps": False,
-                "download_windows_deps": False,
-            },
-        },
-    ]' > .gclient
-    gclient sync
-		
-    cd ${WORKDIR}/src
-    ./flutter/tools/gn ${GN_ARGS}
+FLUTTER_ENGINE_CONFIGURE_CMDS = \
+	exit 1; \
+	export DEPOT_TOOLS_UPDATE=0; \
+	export PATH=${S}:${S}/${GN_TOOLS_PYTHON2_PATH}:$(PATH); \
+	\
+	cd ${WORKDIR}; \
+	echo 'solutions = [ \
+	    { \
+	        "managed" : False, \
+	        "name" : "src/flutter", \
+	        "url" : "https://github.com/flutter/engine.git@${ENGINE_VERSION}", \
+	        "custom_deps": {}, \
+	        "deps_file": "DEPS", \
+	        "safesync_url": "", \
+	        "custom_vars": { \
+	            "download_android_deps": False, \
+	            "download_windows_deps": False, \
+	        }, \
+	    }, \
+	]' > $(D)/.gclient; \
+	gclient sync; \
+	\
+	cd ${WORKDIR}/src; \
+	./flutter/tools/gn ${GN_ARGS}
 
-FLUTTER_ENGINE_COMPILE_CMDS=
-		export PATH=${S}:${S}/${GN_TOOLS_PYTHON2_PATH}:$PATH
+FLUTTER_ENGINE_COMPILE_CMDS = \
+	export PATH=${S}:${S}/${GN_TOOLS_PYTHON2_PATH}:$PATH; \
+	cd ${WORKDIR}/src; \
+	$(NINJA) -C ${ARTIFACT_DIR}
 
-    cd ${WORKDIR}/src
-    ninja -C ${ARTIFACT_DIR}
+FLUTTER_ENGINE_INSTALL_CMDS = \
+	install -d ${D}${libdir}; \
+	install -m 0755 ${WORKDIR}/src/${ARTIFACT_DIR}/libflutter_engine.so ${D}${libdir}
 
-FLUTTER_ENGINE_INSTALL_CMDS=
-    install -d ${D}${libdir}
-    install -m 0755 ${WORKDIR}/src/${ARTIFACT_DIR}/libflutter_engine.so ${D}${libdir}
+$(eval $(generic-package))
+
