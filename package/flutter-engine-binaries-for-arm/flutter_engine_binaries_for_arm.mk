a557a36f94651753d461d4dcde58f7cac5ea8a46
Frank Hunleth <fhunleth@troodon-software.com>
2022-01-12 22:03:45 -0500

    flutter-engine-binaries-for-arm: support arm32

--- package/flutter-engine-binaries-for-arm/flutter_engine_binaries_for_arm.mk
+++ package/flutter-engine-binaries-for-arm/flutter_engine_binaries_for_arm.mk
@@ -12,19 +12,25 @@ FLUTTER_ENGINE_BINARIES_FOR_ARM_INSTALL_STAGING = YES
 FLUTTER_ENGINE_BINARIES_FOR_ARM_LICENSE = BSD-3-Clause
 FLUTTER_ENGINE_BINARIES_FOR_ARM_LICENSE_FILES = LICENSE
 
+ifeq ($(BR2_ARCH_IS_64),y)
+    FLUTTER_ENGINE_BINARIES_ARCH=arm64
+else
+    FLUTTER_ENGINE_BINARIES_ARCH=arm
+endif
+
 define FLUTTER_ENGINE_BINARIES_FOR_ARM_BUILD_CMDS
 	echo "Using Flutter engine binary"
 endef
 
 define FLUTTER_ENGINE_BINARIES_FOR_ARM_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/arm64/libflutter_engine.so.release $(STAGING_DIR)/usr/lib/libflutter_engine.so
-	$(INSTALL) -D -m 0755 $(@D)/arm64/icudtl.dat $(STAGING_DIR)/usr/lib/icudtl.dat
+	$(INSTALL) -D -m 0755 $(@D)/$(FLUTTER_ENGINE_BINARIES_ARCH)/libflutter_engine.so.release $(STAGING_DIR)/usr/lib/libflutter_engine.so
+	$(INSTALL) -D -m 0755 $(@D)/$(FLUTTER_ENGINE_BINARIES_ARCH)/icudtl.dat $(STAGING_DIR)/usr/lib/icudtl.dat
 	$(INSTALL) -D -m 0755 $(@D)/flutter_embedder.h  $(STAGING_DIR)/usr/include
 endef
 
 define FLUTTER_ENGINE_BINARIES_FOR_ARM_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/arm64/libflutter_engine.so.release $(TARGET_DIR)/usr/lib/libflutter_engine.so
-	$(INSTALL) -D -m 0755 $(@D)/arm64/icudtl.dat $(TARGET_DIR)/usr/lib/icudtl.dat
+	$(INSTALL) -D -m 0755 $(@D)/$(FLUTTER_ENGINE_BINARIES_ARCH)/libflutter_engine.so.release $(TARGET_DIR)/usr/lib/libflutter_engine.so
+	$(INSTALL) -D -m 0755 $(@D)/$(FLUTTER_ENGINE_BINARIES_ARCH)/icudtl.dat $(TARGET_DIR)/usr/lib/icudtl.dat
 endef
 
 $(eval $(generic-package))
